a52efcf6fdfa1edb4d47186dc5af1073b9a23458
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-08 05:13:52 +0000

    :white_check_mark: Update tests with parametrization decorator.

--- tests/test_quantum_lstm.py
+++ tests/test_quantum_lstm.py
@@ -1,3 +1,6 @@
+import time
+from datetime import timedelta
+
 import pytest
 import torch
 from flights_time_series_dataset import FlightsDataset
@@ -7,39 +10,27 @@ from time_series_predictor import TimeSeriesPredictor
 MAX_EPOCHS = 50
 
 # @pytest.mark.skip
-def test_quantum_lstm_tsp_fitting():
+# @pytest.mark.parametrize('device', ['cuda', 'cpu'])
+def test_quantum_lstm_tsp_fitting(device='cuda'):
     """
     Tests the Quantum LSTM TimeSeriesPredictor fitting
     """
-    if not torch.cuda.is_available():
-        pytest.skip("needs a CUDA compatible GPU available to run this test")
+    if device == 'cuda':
+        if not torch.cuda.is_available():
+            pytest.skip("needs a CUDA compatible GPU available to run this test")
     tsp = TimeSeriesPredictor(
         QuantumLSTM(),
-        # lr=1E-2,
-        max_epochs=MAX_EPOCHS,
+        lr=1E-1,
+        max_epochs=50,
         train_split=None,
         optimizer=torch.optim.Adam,
+        device=device
     )
 
-
+    start = time.time()
     tsp.fit(FlightsDataset())
-    mean_r2_score = tsp.score(tsp.dataset)
-    assert mean_r2_score > -3
-
-# @pytest.mark.skip
-def test_quantum_lstm_tsp_fitting_in_cpu():
-    """
-    Tests the Quantum LSTM TimeSeriesPredictor fitting in the cpu
-    """
-    tsp = TimeSeriesPredictor(
-        QuantumLSTM(),
-        # lr=1E-2,
-        max_epochs=MAX_EPOCHS,
-        train_split=None,
-        optimizer=torch.optim.Adam,
-        device='cpu'
-    )
-
-    tsp.fit(FlightsDataset())
+    end = time.time()
+    elapsed = timedelta(seconds = end - start)
+    print("Fitting in {} time delta: {}".format(device, elapsed))
     mean_r2_score = tsp.score(tsp.dataset)
     assert mean_r2_score > -3
